1aeaffc8fcedb9c06259255861c49c35743b700a
Salvador Benavidez <bsalvador@hotmail.com>
2024-07-14 16:46:33 -0700

    Main Views

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <main>
+    <RouterView />
+  </main>
   <h1>Welcome to Master Class</h1>
 </template>
 
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  // msg: string
+}>()
+</script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Home</RouterLink>
+  </div>
+</template>
+
+<style scoped></style>
